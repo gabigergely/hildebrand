95ee994624f03ccf4dc82183abf41fa652ad3306
Gabriella Gergely <gabi@purchasingplatform.com>
2014-02-25 11:46:00 -0600

    Basic login/logout functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hildebrand::Application.config.secret_key_base = '733a8732fd6a2117f0a3c3fd29f15d0c2e559cdc04d7d1e9c1ad7ddcae83076b31737f5c2349bf9b3649785599c38fcc78449081b679a77d57e49d189111260d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hildebrand::Application.config.session_store :cookie_store, key: '_hildebrand_session'
